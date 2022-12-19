60dece06ba9fed71e90a040e656eb4adb891da38
ricky <ricky@cloudprodigy.ca>
2022-12-19 09:47:22 -0500

    fixed svc discovery config

--- ecs.tf
+++ ecs.tf
@@ -76,7 +76,7 @@ resource "aws_ecs_service" "this" {
     security_groups = [aws_security_group.ecs.id]
   }
   service_registries {
-    registry_arn = var.enable_service_discovery == "yes" ? one(aws_service_discovery_service.this.*.arn) : null
+    registry_arn = var.enable_service_discovery == "yes" ? aws_service_discovery_service.this[each.key].arn : null
   }
 
   # load_balancer {
